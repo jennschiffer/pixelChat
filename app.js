ee96b781bf0d30b480e0f7c42bf2af4dc1158a0d
Jenn Schiffer <jenn@dotbiz.info>
2015-08-07 18:07:28 -0400

    update express 4 and bodyparser module, css fixes

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
 /* express */
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
 // index
 app.get('/', function(req, resp) {
-    resp.sendfile(__dirname + '/views/index.html');
+  resp.sendfile(__dirname + '/views/index.html');
 });
 
 /* socket.io */
@@ -19,14 +20,14 @@ var io = require('socket.io').listen(server);
 server.listen(3000);
 
 io.sockets.on('connection', function (socket) {
-  
-    socket.on('send', function (data) { 
-    console.log(socket);
-        if ( data.chat) {
-            // send messages
-            io.sockets.emit('message',data);            
-        } 
-    });
-  
-    socket.on('disconnect', function () { });
+
+  socket.on('send', function (data) { 
+  console.log(socket);
+    if ( data.chat) {
+      // send messages
+      io.sockets.emit('message',data);            
+    } 
+  });
+
+  socket.on('disconnect', function () { });
 });
